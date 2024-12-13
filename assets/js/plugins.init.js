b754b27896b0f18634713038deebef0c2453d45c
wouterpie <semrametrep@gmail.com>
2024-12-13 09:22:01 +0700

    first commit

---
+++ assets/js/plugins.init.js
@@ -0,0 +1,370 @@
+/* Template Name: Appever - Tailwind CSS App Landing Template
+   Author: Shreethemes
+   Email: support@shreethemes.in
+   Website: https://shreethemes.in
+   Version: 1.0.0
+   Created: March 2022
+   File Description: Common JS file of the template(plugins.init.js)
+*/
+
+/***********************************/
+/*         INDEX                   */
+/*==================================
+ *     01.  Tobii lightbox         *
+ *     02.  Tiny SLider            *
+ *     03.  Data Counter           *
+ *     04.  Navtabs                *
+ *     05.  Swiper slider          *
+ ==================================*/
+
+//=========================================//
+/*/*            01) Tobii lightbox         */
+//=========================================//
+
+try {
+    const tobii = new Tobii()
+} catch (err) {
+
+}
+
+//=========================================//
+/*/*            02) Tiny SLider            */
+//=========================================//
+if(document.getElementsByClassName('tiny-single-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-single-item',
+        items: 1,
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 0,
+    });
+};
+
+if(document.getElementsByClassName('tiny-one-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-one-item',
+        items: 1,
+        controls: true,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        controlsText: ['<i class="mdi mdi-chevron-left "></i>', '<i class="mdi mdi-chevron-right"></i>'],
+        nav: false,
+        speed: 400,
+        gutter: 0,
+    });
+};
+
+if(document.getElementsByClassName('tiny-three-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-three-item',
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 12,
+        responsive: {
+            992: {
+                items: 3
+            },
+
+            767: {
+                items: 2
+            },
+
+            320: {
+                items: 1
+            },
+        },
+    });
+};
+
+if(document.getElementsByClassName('tiny-four-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-four-item',
+        controls: true,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        controlsText: ['<i class="mdi mdi-chevron-left "></i>', '<i class="mdi mdi-chevron-right"></i>'],
+        nav: false,
+        speed: 400,
+        gutter: 0,
+        responsive: {
+            992: {
+                items: 4
+            },
+
+            767: {
+                items: 3
+            },
+
+            425: {
+                items: 1
+            },
+        },
+    });
+};
+
+//=========================================//
+/*/*            03) Data Counter           */
+//=========================================//
+
+try {
+    const counter = document.querySelectorAll('.counter-value');
+    const speed = 2500; // The lower the slower
+
+    counter.forEach(counter_value => {
+        const updateCount = () => {
+            const target = +counter_value.getAttribute('data-target');
+            const count = +counter_value.innerText;
+
+            // Lower inc to slow and higher to slow
+            var inc = target / speed;
+
+            if (inc < 1) {
+                inc = 1;
+            }
+
+            // Check if target is reached
+            if (count < target) {
+                // Add inc to count and output in counter_value
+                counter_value.innerText = (count + inc).toFixed(0);
+                // Call function every ms
+                setTimeout(updateCount, 1);
+            } else {
+                counter_value.innerText = target;
+            }
+        };
+
+        updateCount();
+    });
+} catch (err) {
+
+}
+
+//=========================================//
+/*/* 03) Typed Text animation (animation) */
+//=========================================//
+
+try {
+    var TxtType = function (el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function () {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+        if (this.isDeleting) { delta /= 2; }
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+        setTimeout(function () {
+            that.tick();
+        }, delta);
+    };
+
+    function typewrite() {
+        if (toRotate === 'undefined') {
+            changeText()
+        }
+        else
+            var elements = document.getElementsByClassName('typewrite');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid transparent}";
+        document.body.appendChild(css);
+    };
+    window.onload(typewrite());
+} catch(err) {
+
+}
+
+//=========================================//
+/*            04) Accordions                  */
+//=========================================//
+try {
+    const Default = {
+        alwaysOpen: false,
+        activeClasses: 'bg-slate-50/50 dark:bg-slate-800/20 text-red-500',
+        inactiveClasses: 'text-slate-900 dark:text-white',
+        onOpen: () => { },
+        onClose: () => { },
+        onToggle: () => { }
+    }
+    
+    class Accordion {
+        constructor(items = [], options = {}) {
+            this._items = items
+            this._options = { ...Default, ...options }
+            this._init()
+        }
+    
+        _init() {
+            if (this._items.length) {
+                // show accordion item based on click
+                this._items.map(item => {
+    
+                    if (item.active) {
+                        this.open(item.id)
+                    }
+    
+                    item.triggerEl.addEventListener('click', () => {
+                        this.toggle(item.id)
+                    })
+                })
+            }
+        }
+    
+        getItem(id) {
+            return this._items.filter(item => item.id === id)[0]
+        }
+    
+        open(id) {
+            const item = this.getItem(id)
+    
+            // don't hide other accordions if always open
+            if (!this._options.alwaysOpen) {
+                this._items.map(i => {
+                    if (i !== item) {
+                        i.triggerEl.classList.remove(...this._options.activeClasses.split(" "))
+                        i.triggerEl.classList.add(...this._options.inactiveClasses.split(" "))
+                        i.targetEl.classList.add('hidden')
+                        i.triggerEl.setAttribute('aria-expanded', false)
+                        i.active = false
+    
+                        // rotate icon if set
+                        if (i.iconEl) {
+                            i.iconEl.classList.remove('rotate-180')
+                        }
+                    }
+                })
+            }
+    
+            // show active item
+            item.triggerEl.classList.add(...this._options.activeClasses.split(" "))
+            item.triggerEl.classList.remove(...this._options.inactiveClasses.split(" "))
+            item.triggerEl.setAttribute('aria-expanded', true)
+            item.targetEl.classList.remove('hidden')
+            item.active = true
+    
+            // rotate icon if set
+            if (item.iconEl) {
+                item.iconEl.classList.add('rotate-180')
+            }
+    
+            // callback function
+            this._options.onOpen(this, item)
+        }
+    
+        toggle(id) {
+            const item = this.getItem(id)
+    
+            if (item.active) {
+                this.close(id)
+            } else {
+                this.open(id)
+            }
+    
+            // callback function
+            this._options.onToggle(this, item)
+        }
+    
+        close(id) {
+            const item = this.getItem(id)
+    
+            item.triggerEl.classList.remove(...this._options.activeClasses.split(" "))
+            item.triggerEl.classList.add(...this._options.inactiveClasses.split(" "))
+            item.targetEl.classList.add('hidden')
+            item.triggerEl.setAttribute('aria-expanded', false)
+            item.active = false
+    
+            // rotate icon if set
+            if (item.iconEl) {
+                item.iconEl.classList.remove('rotate-180')
+            }
+    
+            // callback function
+            this._options.onClose(this, item)
+        }
+    
+    }
+    
+    window.Accordion = Accordion;
+    
+    document.addEventListener('DOMContentLoaded', () => {
+        document.querySelectorAll('[data-accordion]').forEach(accordionEl => {
+    
+            const alwaysOpen = accordionEl.getAttribute('data-accordion')
+            const activeClasses = accordionEl.getAttribute('data-active-classes')
+            const inactiveClasses = accordionEl.getAttribute('data-inactive-classes')
+    
+            const items = []
+            accordionEl.querySelectorAll('[data-accordion-target]').forEach(el => {
+                const item = {
+                    id: el.getAttribute('data-accordion-target'),
+                    triggerEl: el,
+                    targetEl: document.querySelector(el.getAttribute('data-accordion-target')),
+                    iconEl: el.querySelector('[data-accordion-icon]'),
+                    active: el.getAttribute('aria-expanded') === 'true' ? true : false
+                }
+                items.push(item)
+            })
+    
+            new Accordion(items, {
+                alwaysOpen: alwaysOpen === 'open' ? true : false,
+                activeClasses: activeClasses ? activeClasses : Default.activeClasses,
+                inactiveClasses: inactiveClasses ? inactiveClasses : Default.inactiveClasses
+            })
+        })
+    })
+} catch (error) {
+    
+}
